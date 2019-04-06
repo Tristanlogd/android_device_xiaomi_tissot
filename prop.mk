2b2ff9fa6ba76c9b1666d33b3152a7f8b3232266
mountaser <mountaser1998@gmail.com>
2019-04-06 12:22:06 +0530

    tissot: Update props • Enable HAL3 • Enable EIS • Add GCam to aux list • Always use GPU for screen compositing

--- prop.mk
+++ prop.mk
@@ -1,6 +1,9 @@
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+persist.camera.HAL3.enabled=1 \
+persist.camera.eis.enable=1 \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam,com.Wide.GCam,com.Tele.GCam \
 persist.camera.dual.camera=0
 
 # Dalvik
@@ -10,4 +13,8 @@ dalvik.vm.heapgrowthlimit=256m \
 dalvik.vm.heapsize=512m \
 dalvik.vm.heaptargetutilization=0.75 \
 dalvik.vm.heapminfree=4m \
-dalvik.vm.heapmaxfree=8m \
+dalvik.vm.heapmaxfree=8m
+
+# Always use GPU for screen compositing
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.disable_hwc=1
